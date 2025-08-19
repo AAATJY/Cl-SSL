6cc63485032fb0b4f07d1b2a2ba683a8ee01ce62
tjy <a15127220892@163.com>
2025-08-19 20:00:20 +0800

    CL

--- code/train_cl11.py
+++ code/train_cl11.py
@@ -85,9 +85,9 @@ class MPLController:
         return torch.sigmoid(torch.tensor(self.current_trend))  # 趋势越好，权重越大
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--root_path', type=str, default='/home/ubuntu/workspace/Cl-SSL/data/2018LA_Seg_Training Set/', help='Name of Experiment')
-# parser.add_argument('--root_path', type=str, default='/home/zlj/workspace/tjy/MeTi-SSL/data/2018LA_Seg_Training Set/',help='Dataset root path')
-parser.add_argument('--exp', type=str, default='train_cl7_1', help='model_name')
+# parser.add_argument('--root_path', type=str, default='/home/ubuntu/workspace/Cl-SSL/data/2018LA_Seg_Training Set/', help='Name of Experiment')
+parser.add_argument('--root_path', type=str, default='/home/zlj/workspace/tjy/MeTi-SSL/data/2018LA_Seg_Training Set/',help='Dataset root path')
+parser.add_argument('--exp', type=str, default='train_cl11', help='model_name')
 parser.add_argument('--max_iterations', type=int, default=15000, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int, default=2, help='batch_size per gpu')
 parser.add_argument('--labeled_bs', type=int, default=1, help='labeled_batch_size per gpu')
